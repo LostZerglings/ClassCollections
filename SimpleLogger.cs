38255f0899650fd3d71e7fd11233d3a00afa2e7e
LostZerglings <jsutowski@gmail.com>
2017-06-16 10:23:56 -0400

    Adding simple logger class to class collections

---
+++ SimpleLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Alice_Stand_Alone.Class_Storage
+{
+    public class Log
+    {
+        public static void writelog(string text)
+        {
+            string filepath = "your path goes here";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filepath, true))
+                {
+                    writer.WriteLine($"{Environment.UserName} {text} {DateTime.Now}");
+                }
+            }
+            catch (Exception error)
+            {
+                //how you want to handle any errors goes here. 
+            }
+        }
+    }
+
+}

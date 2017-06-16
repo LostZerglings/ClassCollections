06c7be7a98cb418da9621a527a2c5bb1b50e7a3e
LostZerglings <jsutowski@gmail.com>
2017-06-16 10:49:47 -0400

    Notes added to log class

--- SimpleLogger.cs
+++ SimpleLogger.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.IO;
 
-namespace Alice_Stand_Alone.Class_Storage
+namespace Program
 {
     public class Log
     {
         public static void writelog(string text)
         {
+            // First we want to give the class a hard coded path to write logs to. 
+            //
+            // You could make unique user logs by adding Environment.UserName to the path.
+            // Example: path = $@"\\path\to\your\log\folder\{Environment.UserName}log.txt
+            //
+            // Or just log to a single location by specifying a single path.
+            //
+            // Keep in mind this is using a stream writer and we aren't buffering anything so if the file is open
+            // or locked by another process or we try to run it from two locations at the same time we won't be 
+            // able to write the log.
+            
             string filepath = "your path goes here";
 
+            // We can use a try and catch block to catch any errors when writing the log. 
+            
             try
             {
+                // By wrapping the streamwriter in a using block we ensure that it is disposed once we finish writing.
+                //
+                // This helps manage resources and also ensures that we don't lock the file for future logs.
+                //
+                // When creating the streamwriter be sure to append true as the second argument. This allows us 
+                // to append to the log. If you forget this your log will overwrite the existing log each time it runs
+                // resulting in a single (most recent) log being stored instead of a history of logs. 
+                
                 using (StreamWriter writer = new StreamWriter(filepath, true))
                 {
                     writer.WriteLine($"{Environment.UserName} {text} {DateTime.Now}");
